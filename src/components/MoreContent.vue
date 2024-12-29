aba6e13ac5a361ab5af0f4af6d86ceb5e7323e18
zhangyao421 <180969052+zhangyao421@users.noreply.github.com>
2024-12-30 00:46:03 +0800

    Update MoreContent.vue

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="more-content">您可在此编写任意内容</div>
+  <div class="more-content">
+  <p>时间如同沙漏里的细沙，悄无声息地流淌。  这个时间胶囊，记录着我生命中短暂却珍贵的片段。  本月已近尾声，本年也即将结束。  看着这些数字，我仿佛能感受到时间流动的轨迹，也更加体会到人生的短暂与美好。  珍惜每一刻，让时间更有意义。</p>
+  </div>
 </template>
 
 <style lang="scss" scoped>
